23c5f234cfdccb6d7e60d85b6967f2a27fc2c925
TheOceanV <dataguru2016@gmail.com>
2021-10-08 22:58:57 +0530

    add food category carousal

--- client/src/components/FoodTab/index.jsx
+++ client/src/components/FoodTab/index.jsx
@@ -57,6 +57,7 @@ const MobileTabs = () => {
     </>
   );
 };
+
 const LargeTabs = () => {
     const [allTypes, setAllTypes] = useState([
         {
@@ -103,7 +104,7 @@ const LargeTabs = () => {
       const { type } = useParams();
   return (
     <>
-      <div className="hidden lg:flex gap-14 container px-20 mx-auto">
+      <div className="hidden lg:flex gap-14 container px-20 my-8 mx-auto">
       {allTypes.map((items) => (
           <Link to={`/${items.id}`}>
             <div
