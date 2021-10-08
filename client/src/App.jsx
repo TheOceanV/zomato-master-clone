44572f913edffe9c6f963600e842f4f133828ff3
TheOceanV <dataguru2016@gmail.com>
2021-10-08 22:22:46 +0530

    add active food tab ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import HomeLayoutHOC from "./HOC/home.hoc";
 
 // Component
 import Temp from "./components/temp";
+import Master from "./components/master";
 
 function App() {
   return (
     <>
       <HomeLayoutHOC path="/" exact component={Temp} />
+      <HomeLayoutHOC path="/:type" exact component={Master} />
     </>
   );
 }
--- client/src/layout/Home.layout.jsx
+++ client/src/layout/Home.layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 // Components
 import Navbar from "../components/Navbar";
+import FoodTab from "../components/FoodTab";
 
 const HomeLayout = (props) => {
     return (
     <>
       <Navbar />
       <div className="container mx-auto px-4 lg:px-20 ">{props.children}</div> 
+      <FoodTab />
     </>
    );
 };
