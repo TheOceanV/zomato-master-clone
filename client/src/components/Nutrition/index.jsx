7904ac7fce3a70f46308d1687ef5b53bdf5b75aa
TheOceanV <dataguru2016@gmail.com>
2021-10-09 15:04:57 +0530

    add nutrition card

--- client/src/components/Nutrition/index.jsx
+++ client/src/components/Nutrition/index.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 
 // components
+import NutritionHeroCarousal from "./NutritionHeroCarousal";
 import NutritionCarousal from "./NutritionCarousal";
+import NutritionCard from "./NutritionCard";
 
 const Nutrition = () => {
   return (
     <div>
-      <NutritionCarousal />
+      <NutritionHeroCarousal />
+      <div className="my-6">
+        <NutritionCarousal />
+      </div>
+      <div className="flex justify-between flex-wrap ">
+        <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+        />
+        <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+        />
+        <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+        />
+        <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+        />
+        <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+        />
+        <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+        />
+        <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+        />
+      </div>
     </div>
   );
 };
